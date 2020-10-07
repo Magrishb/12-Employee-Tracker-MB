77c54d9d85a8a7144c9686b9d5b125cb59880fc1
Magrishb <bmagrish@gmail.com>
2020-10-06 21:05:26 -0500

    Added table and npm instal mysql

--- EmployeeSeed.sql
+++ EmployeeSeed.sql
@@ -1,3 +1,25 @@
-DROP DATABASE IF EXISTS employess_db;
-CREATE DATABASE employess_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 USE employess_db;
+CREATE TABLE employee
+(
+    id INT NOT NULL
+    AUTO INCREMENT,
+    first-name VARCHAR
+    (30),
+    role_id INT NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY
+    (id)
+);
+CREATE TABLE role
+(
+    id INT REFERENCES employee,
+    
+    first-name VARCHAR
+    (30),
+    role_id INT NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY
+    (id)
+)
